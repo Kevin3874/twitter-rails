24a8d598f1b2e4e80db45402c72b430127c8a039
Kevin3874 <kzhang77@jh.edu>
2024-01-16 17:29:46 -0500

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_152337) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "tweets"
